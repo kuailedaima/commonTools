f102ecfac92f233ef85f89b41bfda8166c3fe91b
王帅豪 <shuaihaome@163.com>
2026-04-01 17:49:34 +0800

    根据环境确定打包是否加仓库前缀

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 通过环境变量判断部署环境
+  base: process.env.GITHUB_ACTIONS ? '/commonTools/' : '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
